c3b220cd33e4822be0e7d6230a92b7d32f1413d3
Marcelo Macedo <mmacedo78@gmail.com>
2024-05-28 11:36:13 -0300

    ...

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,39 @@
+import { IsEmail, IsOptional, IsString, Matches, MinLength } from "class-validator";
+
 export class CreateUserDto {
-    name: string;
-    age: number;
-  }
+
+  /**
+   * User email
+   * @example "user@example.com"
+   */
+  @IsEmail()
+  email: string;
+
+  /**
+   * User password must contain at least 1 number and 1 letter
+   * @example "abc123456"
+   */
+  @IsString()
+  @MinLength(8, { message: 'Password must have length of at least 8' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[a-z]).*$/, {
+    message: 'Password must contain at least 1 number and 1 letter',
+  })
+  password: string;
+
+  /**
+   * User name
+   * @example "John Doe"
+   */
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  /**
+   * User address
+   * @example "World Street 0"
+   */
+  @IsString()
+  @IsOptional()
+  address?: string;
+
+}
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+
+    /**
+     * User current password
+     * @example "abc123456"
+     */
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    currentPassword?: string;
+
+}
